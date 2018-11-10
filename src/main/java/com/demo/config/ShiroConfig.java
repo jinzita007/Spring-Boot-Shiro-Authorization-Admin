4ac19a7a03832690de175bc4089b136aeacf5013
天川之约 <woaitianwen@qq.com>
2018-11-10 14:06:51 +0800

    Shiro配置文件

---
+++ src/main/java/com/demo/config/ShiroConfig.java
@@ -0,0 +1,123 @@
+package com.demo.config;
+
+import com.demo.shiro.MyRealm;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+import org.apache.shiro.mgt.SecurityManager;
+
+import org.apache.shiro.spring.LifecycleBeanPostProcessor;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author zhouyulong
+ * @date 2018/10/30 上午9:14.
+ */
+@Configuration
+public class ShiroConfig {
+    /**
+     * 拦截器
+     * @param securityManager
+     * @return
+     */
+    @Bean
+    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+        System.out.println("ShiroConfiguration.shiroFilter()");
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+
+        // 如果不设置默认会自动寻找Web工程根目录下的"/login.jsp"页面
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        //登录成功后要跳转的链接,不生效
+        //shiroFilterFactoryBean.setSuccessUrl("/index");
+        //未授权页面
+        //这个配置并不会起作用
+        //shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        Map<String,String> filterChainDefinitionMap = new LinkedHashMap<String,String>();
+        // 配置不会被拦截的链接 顺序判断
+        // 定义filterChain，静态资源不拦截
+        filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/logout", "logout");
+        filterChainDefinitionMap.put("/**", "authc");
+
+
+
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        System.out.println("Shiro拦截器工厂类注入成功");
+        return shiroFilterFactoryBean;
+    }
+
+    /**
+     * 凭证匹配器
+     * @return
+     */
+    @Bean
+    public HashedCredentialsMatcher hashedCredentialsMatcher() {
+        HashedCredentialsMatcher hashedCredentialsMatcher = new HashedCredentialsMatcher();
+        hashedCredentialsMatcher.setHashAlgorithmName("md5"); //散列算法
+        hashedCredentialsMatcher.setHashIterations(2); //散列算法，比如散列两次，相当于 md5(md5(""));
+        return hashedCredentialsMatcher;
+    }
+
+    /**
+     * 身份认证realm
+     * @return
+     */
+    @Bean
+    public MyRealm myRealm() {
+        MyRealm myRealm = new MyRealm();
+        myRealm.setCredentialsMatcher(hashedCredentialsMatcher());
+        return myRealm;
+    }
+
+    /**
+     * 安全管理器
+     * @return
+     */
+    @Bean
+    public SecurityManager securityManager() {
+        // 配置SecurityManager，并注入myRealm
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(myRealm());
+        return securityManager;
+    }
+
+    @Bean(name = "lifecycleBeanPostProcessor")
+    public LifecycleBeanPostProcessor lifecycleBeanPostProcessor() {
+        return new LifecycleBeanPostProcessor();
+    }
+
+    /**
+     * shiro开启注解支持
+     * @return
+     */
+    @Bean
+    @DependsOn({"lifecycleBeanPostProcessor"})
+    public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        advisorAutoProxyCreator.setProxyTargetClass(true);
+        return advisorAutoProxyCreator;
+    }
+
+
+    /**
+     * 开启shiro aop注解支持.
+     * @param securityManager
+     * @return
+     */
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager){
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
+    }
+
+
+}
