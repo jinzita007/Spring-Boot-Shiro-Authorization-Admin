07700c538a31e6ec0a102789e9d0e4bca9737304
天川之约 <woaitianwen@qq.com>
2018-11-10 15:46:30 +0800

    用户角色实体类

---
+++ src/main/java/com/demo/model/UserRole.java
@@ -0,0 +1,35 @@
+package com.demo.model;
+
+/**
+ * @author zhouyulong
+ * @date 2018/11/10 下午2:57.
+ */
+public class UserRole {
+    private Integer id;
+    private Integer userId;
+    private Integer roleId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Integer roleId) {
+        this.roleId = roleId;
+    }
+}
